6b6d10acf3e369c5ed201e9c2b06de3e79fafc4b
BETH-A <elizabeth78@me.com>
2018-06-24 15:33:36 -0400

    added styling

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -5,7 +5,7 @@ const VideoListItem = ({video, onVideoSelect}) => {
     // console.log(video);
     const imageUrl = video.snippet.thumbnails.default.url;
     return (
-    <ul onClick={() => onVideoSelect(video)} className="list-group-item"> 
+    <li onClick={() => onVideoSelect(video)} className="list-group-item"> 
         <div className="video-list media">
             <div className="media-left">
                 <img className="media-object" src={imageUrl}/>
@@ -14,7 +14,7 @@ const VideoListItem = ({video, onVideoSelect}) => {
         <div className="media-body">
             <div className="media-heading">{video.snippet.title}</div>
         </div>
-    </ul>
+    </li>
     );
 };
 
